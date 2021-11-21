cc5a655a719b3a28544ae86e4e0f8dcdc85f63a3
César Balderas <cbalderasguillen@gmail.com>
2021-11-21 00:30:17 -0600

    Creación del modelo, carpetas templates y static

--- proyectoIA/EnfAuditivas/models.py
+++ proyectoIA/EnfAuditivas/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Enfermedades(models.Model):
+    nombre = models.CharField(max_length=50)
+    fiebre = models.FloatField(max_length=5)
+    perdAuditiva= models.FloatField(max_length=5)
+    dolorOido = models.FloatField(max_length=5)
+    inflamacion = models.FloatField(max_length=5)
+    mareo = models.FloatField(max_length=5)
+    vertigo = models.FloatField(max_length=5)
+    enrojecimiento = models.FloatField(max_length=5)
+    fatiga = models.FloatField(max_length=5)
+    difComunicacion = models.FloatField(max_length=5)
+    zumbidos = models.FloatField(max_length=5)
+    taponamiento = models.FloatField(max_length=5)
+    antecedentes = models.FloatField(max_length=5)
+    picazon = models.FloatField(max_length=5)
+    traumas = models.FloatField(max_length=5)
+    sangrado = models.FloatField(max_length=5)
+    sumaEnfermedad = models.FloatField(default=0)
+
+
--- proyectoIA/EnfAuditivas/views.py
+++ proyectoIA/EnfAuditivas/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+from .models import Enfermedades
+#from .forms import
 
 # Create your views here.
+def home(request):
+    return render(request,'index.html')
